7624b596e1fee2124a78e106cb32adc8f30dc295
ahmedfawzy <ahmedfawzy.m1996@gmail.com>
2020-11-12 20:55:29 +0200

    Orders intialization

---
+++ views/user/my_orders.php
@@ -0,0 +1,32 @@
+<?php $PAGE_TITLE = "My Orders";
+      $PAGE_Stylesheets = "<link rel='stylesheet' href='../../css/user/my-orders.css'>";
+      $PAGE_scripts = ""; ?>
+
+<?php require_once '../../templates/header.php'; ?>
+<head>
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+<style>
+    body {
+        background-color: aquamarine;
+    }
+    .row div {
+        border: 1px solid #000;
+    }
+</style>
+</head>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-10 offset-1 col-lg-4 offset-lg-0">
+        col1<br>col1<br>col1<br>col1<br>col1<br>col1<br>col1<br>col1<br>col1<br>
+        </div>
+        <div class="col-12 col-lg-8">
+        col2<br>col2<br>col2<br>col2<br>col2<br>col2<br>col2<br>col2<br>col2<br>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php require_once '../../templates/footer.php'; ?>
