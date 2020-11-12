d0d59bca2f04d6f6f2b9ad72260c288135837cd1
Ahmed-Elhadary <ahmedhamamibrahim@gmail.com>
2020-11-12 21:26:13 +0200

    Add Bootstrap To Header

--- templates/header.php
+++ templates/header.php
@@ -5,10 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>header</title>
-    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../../../css/bootstrap.css">
 </head>
 <body>
-    <script src="../js/jquery.js"></script>
-    <script src="../js/bootstrap.bundle.js"></script>
-</body>
-</html>
+ 
