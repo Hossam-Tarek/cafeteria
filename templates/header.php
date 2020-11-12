9b7400d213098b3034c20abed48089fdbd5176f0
marwa gado <mgado148@gmail.com>
2020-11-12 20:42:58 +0200

    add bootstrap css file

--- templates/header.php
+++ templates/header.php
@@ -18,6 +18,7 @@ $user = "admin";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/fontaswesme-all.min.css">
     %STYLE_SHEETS% 
 </head>
