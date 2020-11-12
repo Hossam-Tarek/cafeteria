0b76e868a3922b1e83644e65fd27ca9030703e69
Marwa gado <72562064+marwagado@users.noreply.github.com>
2020-11-12 20:42:25 +0200

    Add header and footer template (#3)

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cafeteria</title>
+<?php 
+$PAGE_TITLE = "My Orders";
+$PAGE_STYLESHEETS = '';
+$PAGE_SCRIPTS = ''; ?> 
 
-    <link rel="stylesheet" href="css/main.css">
-</head>
-<body>
+<?php require("templates/header.php");?>
+
+<?php require("templates/footer.php");?>
 
 
-</body>
-</html>
