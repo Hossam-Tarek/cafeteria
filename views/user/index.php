4b67729b44754af92954c7d0d70a352f0e7a23aa
Hossam Tarek <hossamtarek250@gmail.com>
2020-11-13 23:38:05 +0200

    Add Login system (#10)

---
+++ views/user/index.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+$PAGE_TITLE = "Cafeteria";
+$PAGE_STYLESHEETS = '<link rel="stylesheet" href="/cafeteria/css/user/main.css">';
+$PAGE_SCRIPTS = '';
+?>
+
+<?php require_once "../../templates/header.php" ?>
+
+<?php
+if (isset($_SESSION["success"])) {
+    echo "<p class='success'>". $_SESSION["success"] ."</p>";
+    unset($_SESSION["success"]);
+}
+?>
+
+<?php require_once "../../templates/footer.php" ?>
