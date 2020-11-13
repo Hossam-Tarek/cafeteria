f8c11f5fd6b2726871d7240257b89cfcf38f5bb4
Khalid-Abed <khalidabeed24@gmail.com>
2020-11-13 19:58:21 +0200

    Link main js To Footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,6 @@
 <script src="/cafeteria/js/jquery.js"></script>
 <script src="/cafeteria/js/bootstrap.bundle.js"></script>
+<script src="/cafeteria/js/admin/main.js"></script>
 %PAGE_SCRIPTS%
 <?php ob_end_flush();?> 
 </body>
