0d5d16269858758930fe2ee2b96c5d3d78e9f063
Khalid-Abed <khalidabeed24@gmail.com>
2020-11-13 23:58:29 +0200

    Replace Main js From Footer To Page Scripts

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,5 @@
 <script src="/cafeteria/js/jquery.js"></script>
 <script src="/cafeteria/js/bootstrap.bundle.js"></script>
-<script src="/cafeteria/js/admin/main.js"></script>
 %PAGE_SCRIPTS%
 <?php ob_end_flush();?> 
 </body>
