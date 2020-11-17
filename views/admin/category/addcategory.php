35a6e73f30df9860e31885c3c3e42a9c10b7e4d4
Khalid-Abed <khalidabeed24@gmail.com>
2020-11-17 17:40:18 +0200

    Create Page To Add New Category

---
+++ views/admin/category/addcategory.php
@@ -0,0 +1,64 @@
+<?php
+     $PAGE_TITLE="Add Category";
+     $PAGE_STYLESHEETS = "";
+     $PAGE_SCRIPTS = "<script src='/cafeteria/js/admin/main.js'></script>";
+     require_once  "../../../templates/header.php"; 
+     require_once "../../../database_connection.php";
+?>
+<?php 
+    $nameErr="";
+    // Cleaning of Input    
+    function Clean($data){
+        $data=htmlentities($data);
+        $data=htmlspecialchars($data);
+        $data=trim($data);
+        $data=stripslashes($data);
+        return $data;
+    }
+  if($_SERVER['REQUEST_METHOD']=='POST'){
+    $name=Clean($_POST['name']);
+
+    // Check For Validation 
+    if (empty($_POST["name"])) {
+        $nameErr='Category is Required *';
+    }else{
+        if(preg_match("/^([a-zA-Z' ]+)$/",$name)){
+            $con=new PDO('mysql:host=localhost;dbname=cafeteria;','root','');
+            $stmt2=$con->prepare("SELECT * FROM Category WHERE name=?");
+            $stmt2->execute([$_POST['name']]);
+            $return=$stmt2->fetch();
+            $count=$stmt2->rowCount();
+            if($count > 0){
+                $nameErr='Category Already Existed';
+            }
+        }else{
+            $nameErr='Invalid name given.';
+        }
+    }
+   
+    // Validation Success Then Start Updating 
+    if(empty($nameErr)){
+            $con=new PDO('mysql:host=localhost;dbname=cafeteria;','root','');
+            $stm3=$con->prepare("INSERT INTO Category (name) VALUES (?)");
+            $stm3->execute([$name]);
+            $result=$stm3->fetch();
+            header('Location:allcategories.php');   
+    }
+  }
+    
+?>
+<div class="container">
+    <h1 class="text-center text-secondary my-3">Add New Category </h1>
+    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post" class="form-group" id='form1'>
+        <label for="Category Name">Category Name</label>
+        <input type="text" name="name" class="form-control">
+            <?php if (strlen($nameErr)>0){ ?> 
+                <span class="error text-danger"
+                          style="display:block;float:right;position:relative;
+                          top: -33px;right: 24px;"
+                ><?php echo $nameErr; ?></span>
+            <?php } ?>
+        <input type="submit" name="submit" value="Add Category" class="btn btn-group btn-success my-4">
+    </form>
+</div>
+<?php  require_once  "../../../templates/footer.php"; ?>
