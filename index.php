41f3eaa379638ae870f559d95d5c57b2d4038c58
Marwa gado <72562064+marwagado@users.noreply.github.com>
2020-11-20 01:43:15 +0200

    Add user order and footer (#29)

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 <?php
 
 session_start();
-
 $PAGE_TITLE = "Cafeteria";
 $PAGE_STYLESHEETS = '<link rel="stylesheet" href="/cafeteria/css/main.css">';
 $PAGE_SCRIPTS = "";
+require_once "templates/header.php";
 ?>
 
-<?php require_once "templates/header.php" ?>
+
 
 <?php
-if (isset($_SESSION["success"])) {
-    echo "<p class='success'>". $_SESSION["success"] ."</p>";
-    unset($_SESSION["success"]);
-}
+require_once "templates/footer.php" 
 ?>
-
-<?php require_once "templates/footer.php" ?>
---
+++ views/user/UserOrder.php
@@ -0,0 +1,103 @@
+<?php
+    require_once "../../database_connection.php";
+    session_start();
+    $id;
+    if (isset($_SESSION["email"]) && isset($_SESSION["account-type"])) {
+        if ($_SESSION["account-type"] === "admin") {
+            header("Location: /cafeteria/views/admin/index.php");
+            return;
+        }
+        if ($_SESSION["account-type"] === "user") {
+            $sql = "SELECT user_id FROM User WHERE email = :email";
+            $statement = $conn->prepare($sql);
+            $statement->bindValue(":email", $_SESSION["email"], PDO::PARAM_STR);
+            $statement->execute();
+            $row = $statement->fetch(PDO::FETCH_ASSOC);
+            $id = $row["user_id"];
+        }
+    }
+    class UserOrder
+    {
+        public $query;
+        public $stmt;
+        public $numrows;
+        public $row;
+        //fetch all orders for user
+        public function getOrderData()
+        {
+            global $conn , $id;
+            $dateFrom = str_replace('/', '-', $_GET['dateFrom']); //datepicker in the format of date in sql stmt
+            $dateTo = str_replace('/', '-', $_GET['dateTo']);
+
+            //get price of each order
+            $ordePriceQuery = "SELECT SUM(price*quantity) as price
+                               FROM `Product` INNER JOIN `Order_Product` 
+                               ON Order_Product.product_id=Product.product_id 
+                               INNER JOIN `Order` ON Order_Product.order_id=`Order`.order_id 
+                               AND( date(`Order`.`date`) BETWEEN '" . $dateFrom . "' AND  '" . $dateTo . "')
+                               AND `Order`.`user_id`= $id GROUP BY Order_Product.order_id ORDER BY `Order`.`date` ASC";
+
+            $stmt2 = ($conn)->prepare($ordePriceQuery);
+            $stmt2->execute();
+            $row2 = ($stmt2)->fetchAll(PDO::FETCH_ASSOC);
+
+            $orders =[];   //json object
+            $sum;
+            //get total sum of orders
+            foreach($row2 as $key=>$value){   
+                $sum[] = $value["price"];
+            }
+            $sum = array_sum($sum);
+         
+            $orders["prices"] = $row2;
+            $orders["sum"] = $sum;
+
+            //fetch all orders between 2 dates
+            $this->query = "SELECT * FROM `Order` WHERE user_id = $id
+                            AND date(date) BETWEEN  '" . $dateFrom . "' AND  '" . $dateTo . "'
+                            ORDER BY `date` ASC;"; 
+            $this->stmt = ($conn)->prepare($this->query);
+            ($this->stmt)->execute();
+            $this->numrows = ($this->stmt)->rowCount();
+
+            $this->row = ($this->stmt)->fetchAll(PDO::FETCH_ASSOC);
+            $orders["orders"] = $this->row;
+            return (json_encode($orders));   //return json object
+        }
+    // get products related to order id
+        public function getProducts()
+        {
+            global $conn , $id;
+            $this->query = "SELECT image , name , quantity , price FROM Order_Product,Product 
+                            WHERE Product.product_id=Order_Product.product_id 
+                            AND Order_Product.order_id=" . $_GET["id"] . "";   //id from get method
+            $this->stmt = $conn->prepare($this->query);
+            ($this->stmt)->execute();
+            $this->row = ($this->stmt)->fetchAll(PDO::FETCH_ASSOC);
+            return (json_encode($this->row));
+        }
+
+        //delete order
+        public function deleteOrder()
+        {
+            global $conn;
+            $this->query = "delete from `Order` WHERE
+                            order_id=" . $_GET["deleteOrderId"] . ""; //id from get method
+            $this->stmt = $conn->prepare($this->query);
+            ($this->stmt)->execute();
+        }
+    }
+    $obj = new UserOrder();
+    
+    if (isset($_GET["dateFrom"])){
+       echo($obj->getOrderData());  
+    }
+    if (isset($_GET["id"])) {
+        echo($obj->getProducts());
+    }
+    if (isset($_GET["deleteOrderId"])) {
+        ($obj->deleteOrder());
+    }
+?>
+
+
