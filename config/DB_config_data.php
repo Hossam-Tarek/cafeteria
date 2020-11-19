faece3a1ee68e100c3339ad61d8ce796c20cd88a
ahmedfawzy <ahmedfawzy.m1996@gmail.com>
2020-11-19 22:54:10 +0200

    Add DB user cafeteria

--- config/DB_config_data.php
+++ config/DB_config_data.php
@@ -2,8 +2,8 @@
     abstract class DB_config_data{
         const HOST = 'localhost';
         const BD_NAME = 'cafeteria';
-        const USERNAME = 'root';
-        const PASSWORD = '0000';
+        const USERNAME = 'cafeteria';
+        const PASSWORD = 'itiProject-2020';
         const BDMS_NAME = 'mysql';
     }
     
