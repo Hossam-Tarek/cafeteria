feaa37e80dbc8f85feb42a2a35d2d43cf1f021b1
Khalid-Abed <khalidabeed24@gmail.com>
2020-11-19 13:48:41 +0200

    Add Session To Ensure That You Are Admin Not User

--- views/admin/category/allcategories.php
+++ views/admin/category/allcategories.php
@@ -1,9 +1,15 @@
 <?php
+    session_start();
+    require_once "../../../database_connection.php";
+    
+    if (!isset($_SESSION["account-type"]) || $_SESSION["account-type"] !== "admin") {
+        header("Location: /cafeteria/index.php");
+        return;
+    }
      $PAGE_TITLE="All Categories";
      $PAGE_STYLESHEETS = "";
      $PAGE_SCRIPTS = "<script src='/cafeteria/js/admin/main.js'></script>";
      require_once  "../../../templates/header.php"; 
-     require_once "../../../database_connection.php";
 ?>
 <div class="container">
     <h1 class="text-center my-3">All Categories</h1>
@@ -26,12 +32,11 @@
                 echo "<div class='table-responsive-sm'>";
                     echo "<table class='table'>
                     <tr class='bg-dark text-light'>
-                        <th scope='col'>ID</th>
                         <th scope='col'>Category Name</th>
                         <th scope='col'>Action</th>
                     </tr>";
             foreach($categories as $category){
-                echo "<tr id='".$category['category_id']."'><th scope='row'>".ValidateData($category['category_id'])."</th>".              
+                echo "<tr id='".$category['category_id']."'>".              
                 "<td> ".ValidateData($category['name'])."</td>".
                 "<td>
                 <a data-category=".$category['category_id']." class='deletecategory btn btn-danger mt-4'>Delete</a>
