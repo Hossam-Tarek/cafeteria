88d5ea9ce0eedd09efcdadc984445412fee11890
Ahmed-Elhadary <ahmedhamamibrahim@gmail.com>
2020-11-21 09:05:15 +0200

    Fix Responsive All_Users Page

--- views/admin/user/all_users.php
+++ views/admin/user/all_users.php
@@ -27,35 +27,35 @@ $PAGE_TITLE="All Users";
   try{
       echo "<br>";
       echo "<h1 class=text-center>All Users</h1>";
+      echo "<a href='add-user.php' class='btn btn-success my-3'>Add New Product</a>";
       echo "<br>";    
         $stmt=$conn->prepare("SELECT * FROM `User`");
         $stmt->execute();
         $users=$stmt->fetchAll();
-       if(count($users)>0){     
+       if(count($users)>0){ 
+        echo "<div class='table-responsive-sm'>";    
          echo "<table class='table  table-hover'>
          <thead  >
            <tr class='text-light bg-dark'>
-             <th scope=col>ID</th>
              <th scope=col>Name</th>
-             <th scope=col>Room_id</th>
+             <th scope=col>Room ID</th>
              <th scope=col>Email</th>
-             <th scope=col>Exetra_Info</th>
              <th scope=col>Avatar</th>
              <th scope=col>Actions</th>
            </tr>
          </thead>";
          foreach($users as $user){
-          echo "<tr id='".$user['user_id']."'><th scope='row'>".ValidateData($user['user_id'])."</th>".              
+          echo "<tr id='".$user['user_id']."'>".              
           "<td> ".ValidateData($user['name'])."</td>".
           "<td> ".ValidateData($user['room_id'])."</td>".
           "<td> ".ValidateData($user['email'])."</td>".
-          "<td> ".ValidateData($user['extra_info'])."</td>".
           "<td> "."<img width=100 height=100 style='border-radius:50%' src=".'../../../images/avatars/'.ValidateData($user['avatar']).">"."</td>".
           "<td>
               <a data-user=".$user['user_id']." class='deleteuser btn btn-danger mt-4'>Delete</a>
               <a href='edituser.php?id=".$user['user_id']."' class='btn btn-primary mt-4'>Edit</a></td></tr>";      
           }
      echo "</table>";
+     echo "</div>";
       }    
      }catch(PDOExeption $e){
          echo "Faild To show All users data".$e->getMessage();
