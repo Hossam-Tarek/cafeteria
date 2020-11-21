307d64f4ed8b4f7e7ddbe25eaf4dfe7a29add294
Hossam Tarek <hossamtarek250@gmail.com>
2020-11-21 09:37:29 +0200

    Add image to main page

--- index.php
+++ index.php
@@ -6,8 +6,9 @@ $PAGE_STYLESHEETS = '<link rel="stylesheet" href="/cafeteria/css/main.css">';
 $PAGE_SCRIPTS = "";
 require_once "templates/header.php";
 ?>
-
-
+<div class="img-container">
+    <img src="img/coffee-time.jpg" id="img-background" alt="coffee time">
+</div>
 
 <?php
 require_once "templates/footer.php" 
